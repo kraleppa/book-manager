3562cf6eb88294fedfec3abb9c2148d3d7500230
kraleppa <misiek.nalepa@wp.pl>
2020-05-16 23:54:12 +0200

    website in progress

--- website/script.js
+++ website/script.js
@@ -1,4 +1,5 @@
 const list = document.getElementById("list")
+const dataField = document.getElementById("data-field")
 
 fetch('http://localhost:4000/api/graphql',{
     method: 'POST',
@@ -7,6 +8,7 @@ fetch('http://localhost:4000/api/graphql',{
         query: `
             query{
                 getAllBooks{
+                    id
                     author
                     title
                 }
@@ -19,7 +21,48 @@ fetch('http://localhost:4000/api/graphql',{
     data.data.getAllBooks.forEach(book => {
         const element = document.createElement("li")
         element.className = "list-group-item"
-        element.innerHTML = `${book.title} <br> ${book.author}`
+        element.innerHTML = `${book.title} <button type="button" class="btn btn-success" 
+        onclick=loadBookData(${book.id})>Więcej</button> <br>${book.author} `
         list.appendChild(element)
     })
 })
+
+loadBookData = (id) => {
+    fetch('http://localhost:4000/api/graphql',{
+    method: 'POST',
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({
+        query: `
+            query{
+                getBook(id: "${id}"){
+                    author
+                    title
+                    description
+                    numberOfPages
+                    releaseDate
+                    imageUrl
+                }
+            }
+        `
+    })
+})
+.then(res => res.json())
+.then(data => {
+    let book = data.data.getBook
+    const element = document.createElement("div")
+    element.innerHTML = `
+    <img src="${book.imageUrl}" style = "width: 30rem; height: 40rem">
+                    <table class="table table-striped">
+                        <tbody>
+                            <tr><th>Tytuł:</th><td>${book.title}</td></tr>
+                            <tr><th>Autor:</th><td>${book.author}</td></tr>
+                            <tr><th>Opis:</th><td>${book.description}</td></tr>
+                            <tr><th>Liczba stron:</th><td>${book.numberOfPages}</td></tr>
+                            <tr><th>Dat wydania:</th><td>${book.releaseDate}</td></tr>
+                        </tbody>
+                    </table>
+    `
+    dataField.innerHTML=""
+    dataField.appendChild(element)
+})
+}
