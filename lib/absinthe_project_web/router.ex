a7e6813db6debcaa153d94fed981d9e5a03827cb
kraleppa <misiek.nalepa@wp.pl>
2020-05-16 18:53:47 +0200

    endpoints added

--- lib/absinthe_project_web/router.ex
+++ lib/absinthe_project_web/router.ex
@@ -5,23 +5,13 @@ defmodule AbsintheProjectWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/api", AbsintheProjectWeb do
+  scope "/api" do
     pipe_through :api
-  end
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
+    forward("/graphql", Absinthe.Plug, schema: AbsintheProject.Schema)
 
-    scope "/" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-      live_dashboard "/dashboard", metrics: AbsintheProjectWeb.Telemetry
+    if Mix.env() == :dev do
+      forward("/graphiql", Absinthe.Plug.GraphiQL, schema: AbsintheProject.Schema)
     end
   end
 end
